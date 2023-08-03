4e37308ff664251cd04bfa0ca35e9bf21459a4b8
mMormin <mmboudot@gmail.com>
2023-08-03 18:48:31 +0200

    ADD app.init() + EDIT global logic

--- js/app.js
+++ js/app.js
@@ -1,94 +1,189 @@
-/* Color swap function */
-const slate = document.getElementById("invader");
-const colors = [
-  "pixel__color-black",
-  "pixel__color-blue",
-  "pixel__color-green",
-];
+const app = {
+  /* Global variables */
+  form: document.getElementById("configuration"),
+  slate: document.getElementById("invader"),
+  colors: ["black", "blue", "green", "yellow"],
+  activeColor: "black",
+  gridSize: 8,
+  pixelSize: 30,
 
-/* Game menu display function */
-const gameMenu = function () {
-  const form = document.getElementById("configuration");
-  const palette = document.getElementById("palette");
+  /**
+   * Function init
+   * to initialize the App
+   */
+  init: function () {
+    app.gameMenu();
+    app.gamePalette();
+  },
 
-  /* Input Init */
-  const input = document.createElement("input");
-  input.setAttribute("id", "input");
-  input.setAttribute("type", "text");
-  input.setAttribute("placeholder", "8");
+  /**
+   * Function createInput
+   * to create an input with the params
+   * @param {string} id
+   * @param {string} placeholder
+   * @returns input
+   */
+  createInput: function (id, placeholder = "") {
+    /* Creation and props definition */
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", id);
+    input.setAttribute("placeholder", placeholder);
+    input.className = "input-text";
 
-  /* Button Init */
-  const button = document.createElement("button");
-  button.innerHTML = "GENERATE";
-  button.addEventListener("click", onButtonClick);
+    return input;
+  },
 
-  /* Input and button in DOM */
-  form.append(input, button);
+  /**
+   * Function createButton
+   * to create a button with the his name
+   * @param {string} txt
+   * @returns button
+   */
+  createButton: function (txt = "GENERATE") {
+    /* Button creation and props definition */
+    const button = document.createElement("button");
+    button.textContent = txt;
+    button.className = "input-button";
+    return button;
+  },
 
-  /* On button click function */
-  function onButtonClick(e) {
-    e.preventDefault();
-    let inputValue = document.getElementById("input").value;
-    newSlate(inputValue);
-    if (slate.firstChild) {
-      while (slate.firstChild) {
-        slate.removeChild(slate.firstChild);
+  /**
+   * Function gameMenu
+   * to create the form menu of the game
+   */
+  gameMenu: function () {
+    /* Inputs and button creation */
+    const pixelNumberInput = app.createInput(
+      "nb-pixels",
+      "Taille de la grille"
+    );
+    const pixelSizeInput = app.createInput("size-pixels", "Taille des pixels");
+    const validateButton = app.createButton();
+
+    /* Inputs and buttons to the DOM */
+    app.form.appendChild(pixelNumberInput);
+    app.form.appendChild(pixelSizeInput);
+    app.form.appendChild(validateButton);
+
+    /* Event Listener */
+    app.form.addEventListener("submit", app.handleFormSubmit);
+  },
+
+  /**
+   * Function newSlate
+   * to create slate of the game with columns and pixels
+   */
+  newSlate: function () {
+    // Clean of the slate
+    app.slate.innerHTML = "";
+
+    /* Loop to have "x = gridSize" columns */
+    for (let i = 0; i < app.gridSize; i++) {
+      /* Column Init */
+      const col = document.createElement("div");
+      col.classList.add("col");
+
+      /* Column in DOM */
+      app.slate.appendChild(col);
+
+      /* Loop to have "x = gridSize" pixels */
+      for (let i = 0; i < app.gridSize; i++) {
+        /* Creation and props definition */
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.width = app.pixelSize + "px";
+        pixel.style.height = app.pixelSize + "px";
+
+        /* Pixel to the DOM */
+        col.appendChild(pixel);
+
+        /* Event Listener */
+        pixel.addEventListener("click", app.handlePixelClick);
       }
-      newSlate(inputValue);
     }
-  }
+  },
 
-  for (let i = 0; i < colors.length; i++) {
-    const color = document.createElement("div");
-    color.setAttribute("class", "palette__color");
-    color.classList.add(colors[i]);
-    palette.appendChild(color);
-  }
+  /**
+   * Function gamePalette
+   * to create the palette and display the colors
+   */
+  gamePalette: function () {
+    /* Selector */
+    const palette = document.getElementById("palette");
+
+    /* For each colors, create a button with the defined color */
+    app.colors.forEach(function (color) {
+      /* Button creation and props definition */
+      const colorSwapButton = document.createElement("a");
+      colorSwapButton.className = "palette__color";
+      colorSwapButton.classList.add("pixel__color--" + color);
+      colorSwapButton.dataset.color = color;
+
+      /* Define default color */
+      if (color == app.activeColor) {
+        colorSwapButton.classList.add("active-color");
+      }
+
+      /* Event Listener */
+      colorSwapButton.addEventListener("click", app.handleCurrentColorChange);
+
+      /* Palette to the DOM */
+      palette.appendChild(colorSwapButton);
+    });
+  },
+
+  /**
+   * Function handlePixelClick
+   * to handle the behavior when clicking on a pixel
+   * @param {Event} event
+   */
+  handlePixelClick: function (evt) {
+    const thisPixel = evt.target;
+
+    app.colors.forEach(function (color) {
+      thisPixel.classList.remove("pixel__color--" + color);
+    });
+    thisPixel.classList.add("pixel__color--" + app.activeColor);
+  },
+
+  /**
+   * Function handleFormSubmit
+   * to handle the form validation
+   * @param {PointerEvent} event
+   */
+  handleFormSubmit: function (event) {
+    event.preventDefault();
+
+    /* Selectors */
+    const pixelsNumber = document.getElementById("nb-pixels");
+    const pixelsSize = document.getElementById("size-pixels");
+
+    let pixelsNumberValue = Number(pixelsNumber.value);
+    let pixelsSizeValue = Number(pixelsSize.value);
+
+    if (pixelsNumberValue && pixelsSizeValue) {
+      app.gridSize = pixelsNumberValue;
+      app.pixelSize = pixelsSizeValue;
+      app.newSlate();
+    }
+  },
+
+  /**
+   * Function handleCurrentColorChange
+   * to handle the behavior of the color swapping in the palette
+   * @param {Event} event
+   */
+  handleCurrentColorChange: function (event) {
+    /* Selectors */
+    const oldColor = document.querySelector(".active-color");
+    let newColor = event.target;
+
+    oldColor.classList.remove("active-color");
+    newColor.classList.add("active-color");
+
+    app.activeColor = event.target.dataset.color;
+  },
 };
 
-/* New slate function */
-function newSlate(size) {
-  /* Default value if empty */
-  if (size === "") {
-    size = 8;
-  }
-
-  const colorSwap = function (evt) {
-    const element = evt.target;
-
-    if (element.classList.contains(colors[0])) {
-      element.classList.replace(colors[0], colors[1]);
-    } else if (element.classList.contains(colors[1])) {
-      element.classList.replace(colors[1], colors[2]);
-    } else if (element.classList.contains(colors[2])) {
-      element.classList.replace(colors[2], colors[0]);
-    } else {
-      element.classList.add(colors[0]);
-    }
-  };
-
-  /* Loop to have x = size columns */
-  for (let i = 0; i < size; i++) {
-    /* Column Init */
-    const col = document.createElement("div");
-    col.classList.add("col");
-
-    /* Column in DOM */
-    slate.appendChild(col);
-
-    /* Loop to have x = size pixels */
-    for (let i = 0; i < size; i++) {
-      /* Pixel Init */
-      const pixel = document.createElement("div");
-      pixel.classList.add("pixel");
-
-      /* Pixel in DOM */
-      col.appendChild(pixel);
-
-      pixel.addEventListener("click", colorSwap);
-    }
-  }
-}
-
-/* Game Menu Call */
-gameMenu();
+app.init();
